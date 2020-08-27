2a8ae307d525bac98b8de4b96b8638f86e1527c6
anthony morris <anthonymorris@anthonys-MacBook-Pro.local>
2020-08-27 10:15:10 +0100

    github actions

--- consumer/cmd/main.go
+++ consumer/cmd/main.go
@@ -1,6 +1,6 @@
 package main
 
-// adding commet to trigger github action
+// adding commet to trigger github
 import (
 	"rabbitmq-golang-messaging-example/consumer/pkg/app"
 	"rabbitmq-golang-messaging-example/consumer/pkg/hub"
